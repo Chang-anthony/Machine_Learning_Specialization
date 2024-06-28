a2b10d29b5d293bab782d7558e25800b6fec4874
TonyChang <a23011257@gmail.com>
2024-06-28 21:52:57 +0800

    update setup.py window virtual env .bat configure file

--- venv.bat
+++ venv.bat
@@ -1,25 +1,19 @@
 @echo off
 
-REM 创建虚拟环境
+REM create virtual environment
 python -m venv .venv
 
-REM 激活虚拟环境
-call .venv\Scripts\activate
+REM activate.bat virtual environment
+call .venv\Scripts\activate.bat
 
-REM 安装 wheel 包
-pip install wheel
+REM 安装 wheel 包 and setuptools package
+pip install wheel setuptools
 
-REM 执行 setup.py 构建项目源码分发包
-python setup.py sdist
+python -m pip install --upgrade pip
 
-REM 获取生成的最新的 tar.gz 文件路径
-for /f "tokens=* delims=" %%a in ('dir /b /o:-d dist\machine_learning-*.tar.gz') do set "target=dist\%%a"
+REM use setup.py to install package
+python setup.py install
 
-REM 安装最新生成的 tar.gz 包
-pip install "%target%"
-
-REM 退出虚拟环境
 deactivate
 
-REM 提示用户操作完成
-@REM echo 操作完成。
+
